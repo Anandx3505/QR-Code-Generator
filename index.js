bdd4bcbe24b4796b3e6b0289f3c685ba3b076565
Anand Chaudhary <221030123@juitsolan.in>
2024-01-12 23:56:26 +0530

    Basic QR Code Generator
    Here We created a Basic QR Code Generator using HTML , CSS and JavaScript.
    First Project Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+const qrFormEl = document.getElementById("qrForm");
+const qrImageEl = document.getElementById("qrImage");
+const qrContainerEl = document.getElementById("qrContainer");
+
+
+const renderQRcode = (url) =>{
+    if(!url) return;
+    qrImageEl.src = url;
+    qrContainerEl.classList.add("show");
+}
+
+
+qrFormEl.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+    const formData = new FormData(qrFormEl);
+    const text = formData.get("qrlink");
+    const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
+    
+
+    renderQRcode(qrCodeUrl);
+});
